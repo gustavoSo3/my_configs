d53769fd947980d84681013de4f40440232c5429
Gustavo Sotres <gus.sotres@gmail.com>
2022-09-18 01:58:39 -0500

    Created some common files and some macos files

---
+++ macos/apply.sh
@@ -0,0 +1,6 @@
+ln -sf ./.my_configs/common/.gitconfig ./.gitconfig
+ln -sf ./.my_configs/common/.vimrc ./.vimrc
+ln -sf ./.my_configs/macos/.zprofile ./.zprofile
+ln -sf ./.my_configs/macos/.zshrc ./.zshrc
+
+echo Run "source ./.zshrc" to finish
